002e52fff29bbd4adb230e31309288efc66eaca0
Johan Vrolix <vrxj81@users.noreply.github.com>
2026-06-16 15:56:37 +0200

    Route dim customer model through intermediate model

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -1,28 +1,6 @@
-with customers as (
-  select *
-  from {{ ref('stg_customers') }}
-),
-orders as (
-  select *
-  from {{ ref('stg_orders') }}
-),
-order_rollup as (
-  select
-    customer_id,
-    min(order_date) as first_order_date,
-    max(order_date) as most_recent_order_date,
-    count(*) as order_count
-  from orders
-  group by customer_id
-)
-
 select
-  c.customer_id,
-  c.customer_name,
-  c.email,
-  r.first_order_date,
-  r.most_recent_order_date,
-  coalesce(r.order_count, 0) as order_count
-from customers c
-left join order_rollup r
-  on c.customer_id = r.customer_id
+  customer_id,
+  first_order_date,
+  most_recent_order_date,
+  order_count
+from {{ ref('int_customer_order_activity') }}

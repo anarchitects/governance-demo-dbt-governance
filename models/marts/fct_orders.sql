f44e48182fe0b59d8041528ca91defa4d67443db
Johan Vrolix <vrxj81@users.noreply.github.com>
2026-06-16 15:53:49 +0200

    Route order fact through intermediate model

--- models/marts/fct_orders.sql
+++ models/marts/fct_orders.sql
@@ -1,10 +1,2 @@
-select
-  o.order_id,
-  o.customer_id,
-  o.order_date,
-  o.status,
-  p.amount,
-  p.payment_method
-from {{ ref('stg_orders') }} o
-left join {{ ref('stg_payments') }} p
-  on o.order_id = p.order_id
+select *
+from {{ ref('int_orders_with_payments') }}
